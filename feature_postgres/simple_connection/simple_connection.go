31f48d51ef6ea0a87e4d74d9566837d216875e23
panth37r <pantherbtw@bk.ru>
2026-08-24 02:35:31 +0300

    add postgres connection

---
+++ feature_postgres/simple_connection/simple_connection.go
@@ -0,0 +1,20 @@
+package simpleconnection
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/jackc/pgx/v5"
+)
+
+func CheckConnect() {
+	ctx := context.Background()
+	conn, err := pgx.Connect(ctx, "postgres://postgres:0001@localhost:5432/postgres")
+	if err != nil {
+		panic(err)
+	}
+	if err := conn.Ping(ctx); err != nil {
+		panic(err)
+	}
+	fmt.Println("Подключение к базе данных произошло успешно")
+}
